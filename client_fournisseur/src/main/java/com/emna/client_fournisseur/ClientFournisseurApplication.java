68aa159e2bf741abda3c2c96346e6502e1f80525
emnaTerz <amnaterzi98@gmail.com>
2025-02-06 14:39:34 +0100

    Deuxiéme microservice client fournisseurs

---
+++ client_fournisseur/src/main/java/com/emna/client_fournisseur/ClientFournisseurApplication.java
@@ -0,0 +1,13 @@
+package com.emna.client_fournisseur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientFournisseurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientFournisseurApplication.class, args);
+	}
+
+}
---
+++ client_fournisseur/src/test/java/com/emna/client_fournisseur/ClientFournisseurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emna.client_fournisseur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientFournisseurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
